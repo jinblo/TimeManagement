ee3eea3e7eaa23c552968fbfcc3fd19fc92d2d74
jinblo <katt406@gmail.com>
2024-01-17 11:01:50 +0200

    init

---
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/TimeManagementBeApplication.java
@@ -0,0 +1,13 @@
+package TeamRed.TimeManagementBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagementBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeManagementBeApplication.class, args);
+	}
+
+}
