4b850c80f885c855c8c9777ecc74445b50e165e2
jinblo <katt406@gmail.com>
2024-02-06 10:13:17 +0200

    added react router

--- TimeManagementFE/src/App.jsx
+++ TimeManagementFE/src/App.jsx
@@ -1,7 +1,24 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import ProjectList from "./components/ProjectList"
+import Tab from "./components/Tab"
+
+const router = createBrowserRouter([
+  {
+    element: <Tab />,
+    children: [
+      {
+        path: '/projectlist',
+        element: <ProjectList />
+      },
+    ]
+
+  }
+])
 
 function App() {
-  <ProjectList />
+  return (
+    <RouterProvider router={router} />
+  )
 }
 
-export default App
+export default App;
