9852b3d6e37662c36ee21e672c1d4886e2a5c594
kristinLu <117985817+kristinLu@users.noreply.github.com>
2024-02-06 07:55:20 +0000

    a little bit of EntryRESTController added

--- TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/web/EntryRESTController.java
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/web/EntryRESTController.java
@@ -1,4 +1,23 @@
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
 public class EntryRESTController {
-    
+
+    @Autowired
+	private EntryRepository repository;
+
+    //Kaikkien työaikakirjausten haku
+	@GetMapping("entries")
+	public ResponseEntity<?> getEntries() {
+		try {
+			Iterable<Entry> entries = repository.findAll();
+			if (((List<Entry>) entries).isEmpty()) {
+				return new ResponseEntity<>("Työaikakirjauksia ei löytynyt", HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(entries, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
