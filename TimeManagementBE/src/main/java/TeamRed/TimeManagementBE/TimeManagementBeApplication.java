410583e53d7846c9328e713fc05bce8f13492875
jinblo <katt406@gmail.com>
2024-02-06 09:18:38 +0200

    added project repository and command line runner

--- TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/TimeManagementBeApplication.java
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/TimeManagementBeApplication.java
@@ -1,13 +1,27 @@
 package TeamRed.TimeManagementBE;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import TeamRed.TimeManagementBE.domain.Project;
+import TeamRed.TimeManagementBE.domain.ProjectRepository;
 
 @SpringBootApplication
 public class TimeManagementBeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimeManagementBeApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demoData(ProjectRepository projectRepo) {
+		return (args) -> {
+			projectRepo.save(new Project("Testproject 1"));
+			projectRepo.save(new Project("Testproject 2"));
+			projectRepo.save(new Project("Testproject 3"));
+		};
 	}
 
 }
---
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/domain/ProjectRepository.java
@@ -0,0 +1,7 @@
+package TeamRed.TimeManagementBE.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+}
