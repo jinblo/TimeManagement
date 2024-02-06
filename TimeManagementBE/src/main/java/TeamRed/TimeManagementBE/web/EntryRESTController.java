36b0fa4f24d2b713e774372572a4cbe44e9fa2dd
Kristin Luik <kristin.luik@outlook.com>
2024-02-06 15:33:10 +0200

    Added delete to controllers

--- TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/web/EntryRESTController.java
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/web/EntryRESTController.java
@@ -1,6 +1,7 @@
 package TeamRed.TimeManagementBE.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,10 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import TeamRed.TimeManagementBE.domain.EntryRepository;
+import TeamRed.TimeManagementBE.domain.Project;
 import TeamRed.TimeManagementBE.domain.Entry;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @CrossOrigin
@@ -30,6 +34,21 @@ public class EntryRESTController {
 				return new ResponseEntity<>("Työaikakirjauksia ei löytynyt", HttpStatus.NO_CONTENT);
 			}
 			return new ResponseEntity<>(entries, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	//Työaikakirjauksen poisto
+	@DeleteMapping("entries/{id}")
+	public ResponseEntity<String> removeEntry(@PathVariable("id") Long id) {
+		try {
+			Optional<Entry> removableEntry = repository.findById(id);
+			if (removableEntry.isEmpty()) {
+				return new ResponseEntity<>("Työaikakirjausta ei löytynyt", HttpStatus.NOT_FOUND);
+			}
+			repository.delete(removableEntry.get());
+			return new ResponseEntity<>("Työaikakirjaus poistettu onnistuneesti", HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
--- TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/web/ProjectRESTController.java
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/web/ProjectRESTController.java
@@ -1,6 +1,7 @@
 package TeamRed.TimeManagementBE.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +12,9 @@ import TeamRed.TimeManagementBE.domain.ProjectRepository;
 import TeamRed.TimeManagementBE.domain.Project;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @CrossOrigin
@@ -30,6 +33,21 @@ public class ProjectRESTController {
 				return new ResponseEntity<>("Projekteja ei löytynyt", HttpStatus.NO_CONTENT);
 			}
 			return new ResponseEntity<>(projects, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	//Projektin poisto
+	@DeleteMapping("projects/{id}")
+	public ResponseEntity<String> removeProject(@PathVariable("id") Long id) {
+		try {
+			Optional<Project> removableProject = repository.findById(id);
+			if (removableProject.isEmpty()) {
+				return new ResponseEntity<>("Projektia ei löytynyt", HttpStatus.NOT_FOUND);
+			}
+			repository.delete(removableProject.get());
+			return new ResponseEntity<>("Projekti poistettu onnistuneesti", HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
