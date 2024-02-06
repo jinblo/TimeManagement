d7e0b82dcc494b9d77e5bd7f2141a408f1316fbe
jinblo <katt406@gmail.com>
2024-02-06 09:03:25 +0200

    added project entity

---
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/domain/Project.java
@@ -0,0 +1,42 @@
+package TeamRed.TimeManagementBE.domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String title;
+	
+	public Project() {}
+	
+	public Project(String title) {
+		super();
+		this.title = title;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "Project [id=" + id + ", title=" + title + "]";
+	}
+	
+}
