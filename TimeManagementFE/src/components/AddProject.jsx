908cf6ac3ad5b606d51b248cea2423de4601e131
UllaMontonen <u.montonen@gmail.com>
2024-02-07 19:40:54 +0200

    error handling if title is empty

--- TimeManagementFE/src/components/AddProject.jsx
+++ TimeManagementFE/src/components/AddProject.jsx
@@ -9,34 +9,46 @@ export default function AddProject(props) {
         title: '',
     })
 
+    // error message shown to user
+    const [errorMessage, setErrorMessage] = useState('');
+
     // handling all changes
     const handleChange = (e) => {
-        setProject({ ...project, [e.target.name]: e.target.value})
+        setProject({ ...project, [e.target.name]: e.target.value })
     }
 
     // adding new project to the database
     const addProject = async () => {
-        try {
-            const response = await fetch('http://localhost:8080/projects', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(project),
-            });
 
-            if (response.ok) {
-                // Project added successfully
-                console.log('Project added successfully');
-            } else {
-                // Handle errors
-                console.error('Failed to add project');
+        if (project.title.trim() === "") {
+            setErrorMessage('Nimi ei voi olla tyhjä');
+        } else {
+            try {
+                const response = await fetch('http://localhost:8080/projects', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(project),
+                });
+
+                if (response.ok) {
+                    // Project added successfully
+                    console.log('Project added successfully');
+                    setErrorMessage('');
+                    setProject({title: ''});
+                    props.addProject();
+                    setOpen(false)
+                } else {
+                    // Handle errors
+                    console.error('Failed to add project');
+                    setErrorMessage('Failed to add project');
+                }
+            } catch (error) {
+                console.error('Error adding project:', error);
+                setErrorMessage('Error adding project');
             }
-        } catch (error) {
-            console.error('Error adding project:', error);
         }
-        props.addProject();
-        setOpen(false)
     };
 
     // Handling dialog 
@@ -44,27 +56,28 @@ export default function AddProject(props) {
 
     return (
         <div style={{ marginTop: 20 }}>
-          <Button variant="contained" onClick={() => setOpen(true)} >Lisää uusi projekti</Button>
-          <Dialog open={open} onClose={() => setOpen(false)}>
-            <DialogTitle>Lisää uusi projekti</DialogTitle>
-            <DialogContent>
-              <TextField
-                autoFocus
-                required
-                margin='normal'
-                fullWidth
-                name="title"
-                label="Projekti"
-                type="text"
-                onChange={e => handleChange(e)}
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={() => setOpen(false)}>Peruuta</Button>
-              <Button onClick={addProject}>Tallenna</Button>
-            </DialogActions>
-          </Dialog>
+            <Button variant="contained" onClick={() => setOpen(true)} >Lisää uusi projekti</Button>
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                <DialogTitle>Lisää uusi projekti</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        required
+                        margin='normal'
+                        fullWidth
+                        name="title"
+                        label="Projektin nimi"
+                        type="text"
+                        onChange={e => handleChange(e)}
+                        error={errorMessage !== ''}
+                        helperText={errorMessage}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)}>Peruuta</Button>
+                    <Button onClick={addProject}>Tallenna</Button>
+                </DialogActions>
+            </Dialog>
         </div >
-      )
-    }
-    
+    )
+}
