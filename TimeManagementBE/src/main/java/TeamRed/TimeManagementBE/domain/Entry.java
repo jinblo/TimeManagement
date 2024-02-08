9ec1229689856476e139b1312585a09ff89467d6
Rezin Anton <rezin.anton@gmail.com>
2024-02-08 19:15:16 +0200

    Coomented/Hided Created User, User Repository and ManyToOne in Entry.java

--- TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/domain/Entry.java
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/domain/Entry.java
@@ -30,10 +30,14 @@ public class Entry {
 	@JsonIgnoreProperties({ "entries" })
 	private Project project;
 
-	@ManyToOne
-	@JoinColumn(name = "user_id")
-	@JsonIgnoreProperties({ "userEntries" })
-	private User user;
+	/*
+	 * @ManyToOne
+	 * 
+	 * @JoinColumn(name = "user_id")
+	 * 
+	 * @JsonIgnoreProperties({ "userEntries" })
+	 * private User user;
+	 */
 
 	public Entry() {
 		super();
--- TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/domain/UserRepository.java
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/domain/UserRepository.java
@@ -1,7 +1,9 @@
-package TeamRed.TimeManagementBE.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
+/*
+ * package TeamRed.TimeManagementBE.domain;
+ * 
+ * import org.springframework.data.repository.CrudRepository;
+ * 
+ * public interface UserRepository extends CrudRepository<User, Long> {
+ * 
+ * }
+ */
