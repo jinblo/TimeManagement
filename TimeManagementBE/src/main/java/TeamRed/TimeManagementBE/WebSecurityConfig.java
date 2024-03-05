60c2f1c9e7c536fe9cdf60df0e050ca165895ed9
Kristin Luik <kristin.luik@outlook.com>
2024-03-05 17:43:49 +0200

    Added JWT authentication

--- TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/WebSecurityConfig.java
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/WebSecurityConfig.java
@@ -1,13 +1,21 @@
 package TeamRed.TimeManagementBE;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import TeamRed.TimeManagementBE.web.AppUserDetailsService;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 	
 	@Autowired
@@ -17,4 +25,26 @@ public class WebSecurityConfig {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
+	
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        return authenticationManagerBuilder.build();
+    }
+    
+	
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	http
+    	.csrf(csrf -> csrf.disable())
+    	.authorizeHttpRequests(authorize -> authorize
+    			.requestMatchers(antMatcher("/login")).permitAll()
+    			.anyRequest().authenticated())
+    			//.anyRequest().permitAll())
+    	.sessionManagement(management -> management
+    			.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    	
+        return http.build();
+    }
 }
