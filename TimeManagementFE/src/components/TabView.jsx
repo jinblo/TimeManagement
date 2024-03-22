6da0ecd7a09dd0cb969b13f5d52c9e071f52ba74
jinblo <katt406@gmail.com>
2024-03-22 14:27:49 +0200

    small fixes to TabView, login status alternates shown tabs

--- TimeManagementFE/src/components/TabView.jsx
+++ TimeManagementFE/src/components/TabView.jsx
@@ -3,28 +3,43 @@ import { Box, AppBar, Tabs, Tab, Button, Toolbar, } from '@mui/material';
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import { useAuth } from '../services/AuthProvider';
-import Logout from './Logout';
 
 const TabView = () => {
   const { token } = useAuth()
   let { id } = useParams();
-  const [value, setValue] = useState(0);
+  const [tab, setTab] = useState(0);
+  const [log, setLog] = useState(0);
 
-  const handleChange = (e, val) => {
-    setValue(val);
+  const handleTabChange = (e, val) => {
+    setTab(val);
+  }
+
+  const handleLogChange = (e, val) => {
+    setLog(val)
   }
 
   return (
     <Box>
-      <AppBar position='static'>
+      <AppBar position='static' sx={{ flexGrow: 1 }}>
         <Toolbar>
-          <Tabs value={value} onChange={handleChange}
+          <div style={{ flexGrow: 1 }}>
+            {token ?
+              <Tabs value={tab} onChange={handleTabChange}
+                textColor='inherit' >
+                <Tab component={Link} to={`/`} label='Etusivu' />
+                <Tab component={Link} to={'projectlist'} label='Projektit' />
+                <Tab component={Link} to={'entrylist'} label='Tuntikirjaukset' />
+              </Tabs>
+              : null
+            }
+          </div>
+          <Tabs value={log} onChange={handleLogChange}
             textColor='inherit'>
-            <Tab component={Link} to={`/`} label='Etusivu' />
-            <Tab component={Link} to={'projectlist'} label='Projektit' />
-            <Tab component={Link} to={'entrylist'} label='Tuntikirjaukset' />
-            <Tab component={Link} to={'login'} label='Login' />
-            <Tab component={Link} to={'logout'} label='Logout' />
+            {token ?
+              <Tab component={Link} to={'logout'} label='Kirjaudu ulos' />
+              :
+              <Tab component={Link} to={'login'} label='Kirjaudu sisään' />
+            }
           </Tabs>
         </Toolbar>
       </AppBar>
