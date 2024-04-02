4d078f9c0e6de3b761c367672b08c3b0e6b20ae2
jinblo <katt406@gmail.com>
2024-04-02 11:16:22 +0300

    viimeisimmät development muutokset

--- TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/WebSecurityConfig.java
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/WebSecurityConfig.java
@@ -13,52 +13,66 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 //import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import TeamRed.TimeManagementBE.service.AppUserDetailsService;
+import jakarta.servlet.http.HttpServletRequest;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-	
+
 	@Autowired
 	private AppUserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private AuthenticationFilter authenticationFilter;
-	
+
 	@Autowired
 	private AuthEntryPoint exceptionHandler;
-	
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
-	
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-        return authenticationManagerBuilder.build();
-    }
-    
-	
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    	http
-    	.csrf(csrf -> csrf.disable())
-    	.authorizeHttpRequests(authorize -> authorize
-    			.requestMatchers(antMatcher("/h2-console*")).permitAll()
-    			//.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-    			.requestMatchers(antMatcher("/login")).permitAll()
-    			.requestMatchers(antMatcher(HttpMethod.POST,"/users")).permitAll()
-    			.anyRequest().authenticated())
-    	.sessionManagement(management -> management
-    			.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-    	.exceptionHandling(exception -> exception.authenticationEntryPoint(exceptionHandler))
-    	.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
-    	
-        return http.build();
-    }
+
+	@Bean
+	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+		return authenticationManagerBuilder.build();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(antMatcher("/h2-console*")).permitAll()
+						// .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+						.requestMatchers(antMatcher("/login")).permitAll()
+						.requestMatchers(antMatcher(HttpMethod.POST, "/users")).permitAll()
+						.anyRequest().authenticated())
+				.sessionManagement(management -> management
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.exceptionHandling(exception -> exception.authenticationEntryPoint(exceptionHandler))
+				.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
 }
