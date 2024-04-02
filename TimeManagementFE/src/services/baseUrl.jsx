e97cb16223bad728518caab6f7b1a9545233aa13
jinblo <katt406@gmail.com>
2024-04-02 08:31:48 +0300

    baseurl updated

--- TimeManagementFE/src/services/baseUrl.jsx
+++ TimeManagementFE/src/services/baseUrl.jsx
@@ -1,3 +1,3 @@
-//export const baseUrl = 'http://softala.haaga-helia.fi:8080'
+export const baseUrl = 'http://softala.haaga-helia.fi:8060/'
 
-export const baseUrl = 'http://localhost:8080'
+//export const baseUrl = 'http://localhost:8080'
