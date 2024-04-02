c240fc4f6da8de0d6a7a8fe2a5d983d858563fbb
Kristin Luik <kristin.luik@outlook.com>
2024-04-02 13:28:13 +0300

    SecurityFilterChain updated

--- TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/WebSecurityConfig.java
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/WebSecurityConfig.java
@@ -13,10 +13,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 //import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import TeamRed.TimeManagementBE.service.AppUserDetailsService;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -43,11 +48,24 @@ public class WebSecurityConfig {
         return authenticationManagerBuilder.build();
     }
     
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+    	CorsConfiguration configuration = new CorsConfiguration();
+    	configuration.setAllowedOrigins(Arrays.asList("*"));
+    	configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+    	configuration.setAllowedHeaders(Arrays.asList("*"));
+    	configuration.setAllowCredentials(true);
+    	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    	source.registerCorsConfiguration("/**", configuration);
+    	return source; 	
+    }
+    
 	
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     	http
     	.csrf(csrf -> csrf.disable())
+    	.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
     	.authorizeHttpRequests(authorize -> authorize
     			.requestMatchers(antMatcher("/h2-console*")).permitAll()
     			//.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
