0228492ee1b607a8b93dbe8c2ca6178a41db902f
Kristin Luik <kristin.luik@outlook.com>
2024-04-08 12:55:47 +0300

    Refactored ProjectRESTController, added separate getUserRole method

--- TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/WebSecurityConfig.java
+++ TimeManagementBE/src/main/java/TeamRed/TimeManagementBE/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
     	CorsConfiguration configuration = new CorsConfiguration();
-    	configuration.setAllowedOrigins(Arrays.asList("*"));
+    	configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
     	configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
     	configuration.setAllowedHeaders(Arrays.asList("*"));
     	configuration.setAllowCredentials(true);
